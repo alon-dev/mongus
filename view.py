dbb0dc7ed1b8aa040c02279e04e5df0eec2fba43
alon <alonen15@gmail.com>
2021-10-26 10:28:05 +0300

    mogus

--- view.py
+++ view.py
@@ -1,12 +1,14 @@
 from controller import Controller
-from tkinter import Frame, Menu, Tk, Label, Button, Toplevel, mainloop, messagebox
+from tkinter import Frame, Menu, Tk, Label, Button, Toplevel, mainloop, messagebox, ttk
 import sys
 import os
 from tkinter import Tk, Label, Button, mainloop, messagebox
 
-class MyButton(Button):
+class MyButton(ttk.Button):
     def __init__(self, master, i, j, view):
-        super().__init__(master, text=" ", font=("Helvetica", 20), width=6, height=3, state="disabled", command = self.callback)
+        s = ttk.Style()
+        s.configure('my.TButton', font=('Helvetica', 20))
+        super().__init__(master, text=" ", style="my.TButton", state="disabled", command = self.callback)
         self._i = i
         self._j = j
         self._view = view
@@ -120,11 +122,13 @@ class View:
     def __init__(self, master):
         self._controller = Controller()
         self.master = master
-        a = Frame(master)
-        self.button = Button(a, text= "VS PC", command= self.callback1)
-        self.button1 = Button(a, text= "VS Person", command= self.callback2)
-        self.button.pack()
-        self.button1.pack()
+        a = ttk.Frame(master)
+        left_a = ttk.Frame(a)
+        left_a.grid(column=0)
+        self.button = ttk.Button(left_a, text= "VS PC", command= self.callback1)
+        self.button1 = ttk.Button(left_a, text= "VS Person", command= self.callback2)
+        self.button.pack(expand=True, fill='both')
+        self.button1.pack(expand=True, fill='both')
 
         b = Frame(master)
         self.turnx = True
@@ -139,29 +143,32 @@ class View:
         self.b7 = MyButton(b, 2, 0, self)
         self.b8 = MyButton(b, 2, 1, self)
         self.b9 = MyButton(b, 2, 2, self)
+        right_a = ttk.Frame(a)
+        right_a.grid(column=1)
+        self.mainLabel = Label(right_a, text= "It is now x's turn", font=("Helvetica", 20))
+        s = ttk.Style()
+        s.configure('my.TButton', font=('Helvetica', 20))
+        self.resetButton = ttk.Button(right_a, text="Reset Game", style='my.TButton', command=self.restart)
 
-        self.mainLabel = Label(b, text= "It is now x's turn", font=("Helvetica", 20))
-        self.resetButton = Button(b, text="Reset Game", font=("Helvetica", 20), command=self.restart)
-
-        self.b1.grid(row=0, column=0)
-        self.b2.grid(row=0, column=1)
-        self.b3.grid(row=0, column=2)
+        self.b1.grid(row=0, column=0, ipadx=20, ipady=40)
+        self.b2.grid(row=0, column=1 ,ipadx=20, ipady=40)
+        self.b3.grid(row=0, column=2, ipadx=20, ipady=40)
         
-        self.b4.grid(row=1, column=0)
-        self.b5.grid(row=1, column=1)
-        self.b6.grid(row=1, column=2)
+        self.b4.grid(row=1, column=0, ipadx=20, ipady=40)
+        self.b5.grid(row=1, column=1, ipadx=20, ipady=40)
+        self.b6.grid(row=1, column=2, ipadx=20, ipady=40)
 
-        self.b7.grid(row=2, column=0)
-        self.b8.grid(row=2, column=1)
-        self.b9.grid(row=2, column=2)
+        self.b7.grid(row=2, column=0, ipadx=20, ipady=40)
+        self.b8.grid(row=2, column=1, ipadx=20, ipady=40)
+        self.b9.grid(row=2, column=2, ipadx=20, ipady=40)
         b.pack()
         a.pack()
         self.buttons = []
         self.buttons.append([self.b1, self.b2, self.b3])
         self.buttons.append([self.b4, self.b5, self.b6])
         self.buttons.append([self.b7, self.b8, self.b9])
-        self.mainLabel.grid(row=3, columnspan=3)
-        self.resetButton.grid(row=4, columnspan=3)
+        self.mainLabel.pack()
+        self.resetButton.pack()
         w = Menu(master)
         w.add_command(label="About", command= self.callback)
         self.master.config(menu=w)
